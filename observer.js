656679c697bfb2c1ff1074a1f04f20fe1aa053f5
Diogo Bessa <diogobessawd@gmail.com>
2019-12-22 14:32:39 +0000

    add addObserver, removeObserver, notify function in Subject class

--- observer.js
+++ observer.js
@@ -21,10 +21,21 @@ class ObserverList {
 class Subject {
 
     constructor(){
-
+        this.observers = new ObserverList();
     }
 
     addObserver = () => {
-        
+        this.observers.add( observer );
+    }
+
+    removeObserver = observer => {
+        this.observers.removeAt( this.observers.indexOf( observer, 0 ) );
+    }
+       
+    notify = context => {
+        const observerCount = this.observers.count();
+        for(let i=0; i < observerCount; i++){
+            this.observers.get(i).update( context );
+        }
     }
 }
